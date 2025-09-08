c6561db0070437f36889f9ed92496d91cddaada2
kleiton ferreira <kleiton.silva@caraguatatuba.sp.gov.br>
2025-09-08 14:07:01 -0300

    conversor está operando

--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -1,44 +1,54 @@
-<?
+<?php
 
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\LoadFileService;
 
-class ConversorController extends Controller
-{
+class ConversorController extends Controller {
     public array $data;
     public $xml;
+    public $namespace;
 
     public function __construct() {
-
         //Carrega dados da planilha
         $this->data = (new LoadFileService())->reader();
+    }
+
+
+    
+    //adiciona os dados
+    public function addClassificados() {
 
         //carrega padrão AUDESP XML
-        $this->xml = simplexml_load_file("conversor_xml/resources/views/xml/xmlPattern.xml");
-    }
-    
-    //acessa o nó onde adicionar os dados
-    public $listaClassificados = $this->xml->DadosClassificacao->Classificados->children();
+        $this->xml = simplexml_load_file(base_path("resources/views/xml/xmlPattern.xml"));
 
-    //adiciona os dados
-    public function addClassificados($listaClassificados, $data) {
+        $this->namespace = $this->xml->getNamespaces(true);
+
+        // Acessa os nós usando os namespaces corretamente
+        $classificacao = $this->xml->children($this->namespace['cpe'])->Classificacao ?? null;
+        $classificados = $classificacao->children($this->namespace['cpe'])->Classificados ?? null;
+        $listaClassificados = $classificados->children($this->namespace['lcl']);
+        $data = $this->data;
+
         
         foreach ($listaClassificados as $classificado) {
             
-            $classificado->cpfClassificado.$data["S"];
-            $classificado->nomeClassificado.$data["T"];
-            $classificado->ordemClassificacao.$data["U"];
+            $classificado->cpfClassificado = $data[0]["S"];
+            $classificado->nomeClassificado = $data[1]["T"];
+            $classificado->ordemClassificacao = $data[2]["U"];
         } 
 
-        for($i = 1; $i < count($data); $i++) {
-            $this->listaClassificados->addChild("Classificado")->addChild("cpfClassificado", $data[$i]["S"]);
-            $this->listaClassificados->addChild("Classificado")->addChild("nomeClassificado", $data[$i]["T"]);
-            $this->listaClassificados->addChild("Classificado")->addChild("ordemClassificacao", $data[$i]["U"]);
+        for ($i = 1; $i < count($data); $i++) {
+            $novo = $listaClassificados->addChild("lcl:Classificado", null, $this->namespace['lcl']);
+            $cpf = $novo->addChild("lcl:cpfClassificado", null, $this->namespace['lcl']);
+            $cpf->addChild("gen:Numero", $data[$i]["S"], $this->namespace['gen']);
+
+            $novo->addChild("lcl:nomeClassificado", $data[$i]["T"], $this->namespace['lcl']);
+            $novo->addChild("lcl:ordemClassificacao", $data[$i]["U"], $this->namespace['lcl']);
         }
 
-        $this->xml->asXML(storage_path("conversor_xml/resources/views/xml/xmlConverted.xml"));
+        $this->xml->asXML(base_path("resources/views/xml/xmlConverted.xml"));
         dd($this->xml);
     }
     
