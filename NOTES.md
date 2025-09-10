b5518cbb2742a71c8140f1958ca90697edd626e1
kleiton ferreira <kleiton.silva@caraguatatuba.sp.gov.br>
2025-09-10 12:19:01 -0300

    Adiciona documentação sobre a nova planilha e atualiza o controlador para refletir as mudanças nos campos e dados do XML

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+A nova planilha continha mais campos em relação a outra, foi preciso mudar as referencias em linha de conversão;
+
+A nova planilha contém cabeçalho, o código atual já interpreta a primeira linha como dados a ser inseridos (exclui cabeçalho);
+
+É preciso que na pasta do excel, os dados a ser convertidos estejam na primeira planilha.
+
+Na planilha, percebi que o cód da entidade prevista e o código cargo estavam discrepante do exemplo do arquivo da AUDESP. (troquei na planilha)
--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -16,6 +16,9 @@ class ConversorController extends Controller {
     }
 
     public function addClassificados() {
+
+    // Dados da planilha
+    $data = $this->data;
     
     // Caminho para o arquivo XML
     $xmlFilePath = base_path("storage/xml/xmlPattern.xml");
@@ -34,7 +37,7 @@ class ConversorController extends Controller {
 
     // Descritor do XML
     $descritor = $this->xml->children($this->namespace['cpe'])->Descritor ?? null;
-    $descritor->children($this->namespace['gen'])->DetaCriacaoXML = date('Y-m-d');
+    $descritor->children($this->namespace['gen'])-> DataCriacaoXML = date('Y-m-d');
 
     // IdentificacaoConcursoPublicoEfetivo do XML
     $identificacaoConcursoPublicoEfetivo = $this->xml->children($this->namespace['cpe'])->IdentificacaoConcursoPublicoEfetivo ?? null;
@@ -47,41 +50,60 @@ class ConversorController extends Controller {
     $dadosConcursoPublicoEfetivo->children($this->namespace['cpe'])->pctVagasEspeciaisDef = $this->data[0]["I"];
     $dadosConcursoPublicoEfetivo->children($this->namespace['cpe'])->codigoFatorArredondamento = $this->data[0]["J"];
 
-    $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->prazoValidadeInicial ?? null;
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->anoPrazo = $this->data[0]["K"];
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->mesPrazo = $this->data[0]["L"];
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->diaPrazo = $this->data[0]["M"];
+    $prazoValidadeInicial = $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->prazoValidadeInicial ?? null;
+    $prazoValidadeInicial->children($this->namespace["ap"])->anoPrazo = $this->data[0]["K"];
+    $prazoValidadeInicial->children($this->namespace["ap"])->mesPrazo = $this->data[0]["L"];
+    $prazoValidadeInicial->children($this->namespace["ap"])->diaPrazo = $this->data[0]["M"];
 
-    $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->prazoPrevistoProrrogacao ?? null;
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->anoPrazo = $this->data[0]["N"];
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->mesPrazo = $this->data[0]["O"];
-    $dadosConcursoPublicoEfetivo->children($this->namespace["ap"])->diaPrazo = $this->data[0]["P"];
+    $prazoPrevistoProrrogacao = $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->prazoPrevistoProrrogacao ?? null;
+    $prazoPrevistoProrrogacao->children($this->namespace["ap"])->anoPrazo = $this->data[0]["N"];
+    $prazoPrevistoProrrogacao->children($this->namespace["ap"])->mesPrazo = $this->data[0]["O"];
+    $prazoPrevistoProrrogacao->children($this->namespace["ap"])->diaPrazo = $this->data[0]["P"];
 
-    $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->edital ?? null;
-    $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->dataPublicacaoEdital = $this->data[0]["Q"];
-    $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->meioPublicacaoEdital = $this->data[0]["R"];
+    $edital = $dadosConcursoPublicoEfetivo->children($this->namespace["cpe"])->edital ?? null;
+    $edital->children($this->namespace["cpe"])->dataPublicacaoEdital = $this->data[0]["Q"];
+    $edital->children($this->namespace["cpe"])->meioPublicacaoEdital = $this->data[0]["R"];
 
 
 
     // Lista de cargos do XML
-    $listaCargos = $this->xml->children($this->namespace['cpe'])-> CargoEdital ?? null;
-    $listaCargos->children($this->namespace['ap'])->CodigoCargo = $this->data[1]["U"];
-    $listaCargos->children($this->namespace['ap'])->EntidadePrevista;
-    $listaCargos->children($this->namespace['ap'])->CodigoEntidadePrevista = $this->data[0]["S"];
-    $listaCargos->children($this->namespace['ap'])->CargoMunicipioEntidadePrevista = $this->data[0]["T"];
+    $listaCargos = $this->xml->children($this->namespace['cpe'])-> ListaCargos ?? null;
+    $cargo = $listaCargos->children($this->namespace['cpe'])-> Cargo ?? null;
+    $cargoEdital = $cargo->children($this->namespace['cpe'])-> CargoEdital ?? null;
+ 
+    $entidadePrevista = $cargoEdital->children($this->namespace['ap'])-> EntidadePrevista;
+    $entidadePrevista->children($this->namespace['ap'])->CodigoEntidadePrevista = $this->data[0]["S"];
+    $entidadePrevista->children($this->namespace['ap'])->CodigoMunicipioEntidadePrevista = $this->data[0]["T"];
 
-    $listaCargos->children($this->namespace['cpe'])->permiteAtribPontoTitulo = $this->data[0]["V"];
-    $listaCargos->children($this->namespace['cpe'])->numVagasCargoFuncao = $this->data[0]["W"];
+    $cargoEdital->children($this->namespace['ap'])-> codigoCargo = $this->data[0]["U"];
 
-    // a finalizar restante dos campos de cargos
-
-
+    $cargo->children($this->namespace['cpe'])->permiteAtribPontoTitulo = $this->data[0]["V"];
+    $cargo->children($this->namespace['cpe'])->numVagasCargoFuncao = $this->data[0]["W"];
+   
     // Classificados do XML
-    $classificacao = $this->xml->children($this->namespace['cpe'])->Classificacao ?? null;
-    $classificados = $classificacao->children($this->namespace['cpe'])->Classificados ?? null;
+    $classificacao = $this->xml->children($this->namespace['cpe'])-> Classificacao ?? null;
+    $dadosClassificacao = $classificacao->children($this->namespace['cpe'])-> DadosClassificacao ?? null;
+    $cargoFuncaoEdital = $dadosClassificacao->children($this->namespace['lcl'])-> CargoFuncaoEdital ?? null;
+    $entidadePrevista = $cargoFuncaoEdital->children($this->namespace['ap'])-> EntidadePrevista ?? null;
+    $entidadePrevista->children($this->namespace['ap'])-> CodigoEntidadePrevista = $this->data[0]["X"];
+    $entidadePrevista->children($this->namespace["ap"])-> CodigoMunicipioEntidadePrevista = $this->data[0]["Y"];
 
-    $data = $this->data;
+    $cargoFuncaoEdital->children($this->namespace['ap'])-> codigoCargo = $this->data[0]["Z"];
 
+    $dadosClassificacao->children($this->namespace['lcl'])-> dataPublicacaoListaClassificacao = $this->data[0]["AA"];
+    $dadosClassificacao->children($this->namespace['lcl'])-> dataAtoHomologacaoConcurso = $this->data[0]["AB"];
+    $dadosClassificacao->children($this->namespace['lcl'])-> dataValidadeInicial = $this->data[0]["AC"];
+    $dadosClassificacao->children($this->namespace['lcl'])-> dataPublicacaoHomologacao = $this->data[0]["AD"];
+    
+    $cpfResponsavelHomologacao = $dadosClassificacao->children($this->namespace['lcl'])-> cpfResponsavelHomologacao ?? null;
+    $cpfResponsavelHomologacao->children($this->namespace['gen'])-> Numero = $this->data[0]["AF"];
+    $dadosClassificacao->children($this->namespace['lcl'])-> codigoCargoResponsavelHomologacao = $this->data[0]["AG"];
+    $dadosClassificacao->children($this->namespace['lcl'])-> codigoFuncaoResponsavelHomologacao = $this->data[0]["AH"];
+
+    $classificados = $classificacao->children($this->namespace['cpe'])-> Classificados ?? null;
+
+   
+    // Adiciona os classificados do array ao XML
     for ($i = 0; $i < count($data); $i++) {
         $novo = $classificados->addChild('lcl:Classificado', null, $this->namespace['lcl']);
         $cpf = $novo->addChild('lcl:cpfClassificado', null, $this->namespace['lcl']);
@@ -95,6 +117,17 @@ class ConversorController extends Controller {
         $novo->addChild('lcl:nomeClassificado', $data[$i]["AJ"], $this->namespace['lcl']);
         $novo->addChild('lcl:ordemClassificacao', $data[$i]["AK"], $this->namespace['lcl']);
     }
+
+
+    // Dados do prazo de prorrogação do XML
+    $prazoProrrogacao = $this->xml->children($this->namespace['cpe'])-> PrazoProrrogacao ?? null;
+    $dtPrazoProrrogValidade = $prazoProrrogacao->children($this->namespace['pro'])-> dtPrazoProrrogValidade ?? null;
+    $dtPrazoProrrogValidade->children($this->namespace['ap'])-> anoPrazo = $this->data[0]["N"];
+    $dtPrazoProrrogValidade->children($this->namespace['ap'])-> mesPrazo = $this->data[0]["O"];
+    $dtPrazoProrrogValidade->children($this->namespace['ap'])-> diaPrazo = $this->data[0]["P"];
+
+
+    
 
     // Salva o XML modificado temporariamente em string
     $xmlString = $this->xml->asXML();
